edc5e85a83e9670e4c4a7a9319bdc0e71110d6ee
Ahmad Anbarje <ahmad.a.93@hotmail.com>
2021-06-15 11:49:41 +0200

    test

--- backend_nodeJs/index.js
+++ backend_nodeJs/index.js
@@ -8,6 +8,7 @@ app.use(bodyparser.json());
 
 
 
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
